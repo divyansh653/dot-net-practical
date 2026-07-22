af01a76952ac4f6999bd2cd9602a8d1f86d6d604
Divyansh <divyanshmate2@gmail.com>
2026-07-22 22:12:14 +0530

    Add files via upload

---
+++ Assignments/EmplyoeeSystem_MVC/DepartmentController.cs
@@ -0,0 +1,21 @@
+﻿using EmployeeManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            Department department = new Department()
+            {
+                DepartmentName = "Computer Science",
+                DepartmentHead = "Dr. Rajesh Sharma",
+                HeadContactNumber = "9876543210",
+                HeadEmail = "hod.cse@ssgmce.ac.in"
+            };
+
+            return View(department);
+        }
+    }
+}
