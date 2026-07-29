b8230a438e50f6732860f70e73b98e6fee5a7c03
Divyansh <divyanshmate2@gmail.com>
2026-07-29 23:41:20 +0530

    Add files via upload

---
+++ Practice/25_july/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace _25_july.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int Age { get; set; }
+        public string Department { get; set; } = string.Empty;
+
+    }
+}
