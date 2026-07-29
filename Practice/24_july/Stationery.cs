1f3eacd99b9fbc13a8a746fb5130752ee8742e22
Divyansh <divyanshmate2@gmail.com>
2026-07-29 23:40:00 +0530

    Add files via upload

---
+++ Practice/24_july/Stationery.cs
@@ -0,0 +1,11 @@
+﻿namespace _24_july.Models
+{
+    public class Stationery
+    {
+        public string ItemName { get; set; }
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ Practice/24_july/Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _24_july.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; } = String.Empty;
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; } = String.Empty;
+    }
+}
