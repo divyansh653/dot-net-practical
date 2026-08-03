72834663233f3e298ec1dd61bcd91746f3e71d39
Divyansh <divyanshmate2@gmail.com>
2026-08-03 22:20:36 +0530

    Delete Assignments/E-commerce/CartItem.cs

--- Assignments/E-commerce/CartItem.cs
+++
@@ -1,9 +0,0 @@
-//Store products added to the cart
-public class CartItem
-{
-    public string ProductName { get; set; } = "";
-
-    public int Quantity { get; set; }
-
-    public double Price { get; set; }
-}
