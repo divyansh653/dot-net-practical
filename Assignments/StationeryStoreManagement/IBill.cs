3a45d02a458a6e91e2c6f6f0f9b8a3dd33908916
Divyansh <divyanshmate2@gmail.com>
2026-08-03 22:37:55 +0530

    Delete Assignments/StationeryStoreManagement/IBill.cs

--- Assignments/StationeryStoreManagement/IBill.cs
+++
@@ -1,7 +0,0 @@
-namespace StationeryStoreManagement.Interfaces
-{
-    public interface IBill
-    {
-        void GenerateBill(string itemName, double price, int quantity, double discount, double gst, double total);
-    }
-}
