ce2783c1a04893af188ebe1238015db54e399578
Divyansh <divyanshmate2@gmail.com>
2026-08-03 22:27:54 +0530

    Delete Assignments/ShopeEase/Admin.cs

--- Assignments/ShopeEase/Admin.cs
+++
@@ -1,9 +0,0 @@
-namespace ShopEase.Models
-{
-    public class Admin
-    {
-        public string Username { get; set; } = "admin";
-
-        public string Password { get; set; } = "admin123";
-    }
-}
